a942fa08b2d700474b40501dfa8b36ca579136cc
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2024-01-23 19:49:19 -0500

    Added print  statement so we know why it  trys to run people over

--- src/main/java/frc/robot/commands/SwerveMoveToCMD.java
+++ src/main/java/frc/robot/commands/SwerveMoveToCMD.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.math.controller.PIDController;
@@ -73,7 +74,9 @@ public class SwerveMoveToCMD extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    DriverStation.reportWarning("POINT MOVE ENDED", false);
+  }
 
   // Returns true when the command should end.
   @Override
