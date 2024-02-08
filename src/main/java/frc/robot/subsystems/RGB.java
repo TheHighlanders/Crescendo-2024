4b1a9d6d3a3241ac68445b2aa1d81ca639c2f821
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2024-02-08 16:23:04 -0500

    Added LED changing enum functions

--- src/main/java/frc/robot/subsystems/RGB.java
+++ src/main/java/frc/robot/subsystems/RGB.java
@@ -4,31 +4,62 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import java.util.Optional;
+import java.util.EnumMap;
 
 public class RGB extends SubsystemBase {
 
     /** Creates a new RGB. */
     private SerialPort elPuerto;
 
+    enum State {
+        TEST,
+        OFF,
+        RED,
+        BLUE,
+        RAINBOW,
+        ORANGEBLINK,
+        ORANGESOLID,
+    }
+
+    public EnumMap<State, String> stateMap = new EnumMap<>(State.class);
 
     public RGB() {
-      elPuerto = new SerialPort(9600, SerialPort.Port.kMXP);
-      elPuerto.writeString("5\n");
+        elPuerto = new SerialPort(9600, SerialPort.Port.kMXP);
+        stateMap.put(State.TEST, "0");
+        stateMap.put(State.OFF, "1");
+        stateMap.put(State.RED, "2");
+        stateMap.put(State.BLUE, "3");
+        stateMap.put(State.RAINBOW, "4");
+        stateMap.put(State.ORANGEBLINK, "5");
+        stateMap.put(State.ORANGESOLID, "6");
+
+        elPuerto.writeString(stateMap.get(State.OFF));
     }
 
     public void changeString(String str) {
-      elPuerto.writeString(str + "\n");
+        elPuerto.writeString(str + "\n");
+    }
+
+    public void setLED(State state) {
+        changeString(stateMap.get(state));
+    }
+
+    /**
+     *
+     * @param angleDif Angle Difference between arm and pivot !DEG! 100DEG is max
+     */
+    public void setArmLEDLoadingBar(double angleDif) {
+        angleDif = Math.max(Math.min(Math.abs(angleDif), 100), 0);
+
+        int state = ((int) angleDif) + 100;
+        changeString(state + "");
     }
 
     @Override
     public void periodic() {
-
-      // DriverStation.reportWarning("something", false);
+        // DriverStation.reportWarning("something", false);
         // This method will be called once per scheduler run
     }
 }
